05dbc0b3e9a782fcf96f5c5a5b6f85f523a62bb2
S0meDev99 <90775954+S0meDev99@users.noreply.github.com>
2021-11-11 19:30:54 +0800

    Update connect.php
    connect.php for establishing connection with LAMP server database.
    -As long as your db-config.ini is configured correctly, it should work.

--- connect.php
+++ connect.php
@@ -1,17 +1,11 @@
 <?php
 	// Database connection variables and credentials
         
-	//$config = parse_ini_file('../../private/db-config.ini');
-	//$servername = $config['servername'];
-	//$username = $config['username'];
-	//$password = $config['password'];
-	//$dbname = $config['dbname'];
-	
-	//Temporary variables for development
-	$servername = "35.198.206.85"; 
-	$username = "sqldev";
-	$password = "Ict1004#";
-	$dbname = "double_o4_bank";
+	$config = parse_ini_file('../../private/db-config.ini');
+	$servername = $config['servername'];
+	$username = $config['username'];
+	$password = $config['password'];
+	$dbname = $config['dbname']; // In db-config.ini [dbname = "double_o4_bank"]
         
 	try {
 		$connect = new PDO( "mysql:host=$servername;dbname=$dbname", $username, $password);
