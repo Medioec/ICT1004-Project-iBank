d0ca644a7f1ad605f5897ead441ee6eeffe870b7
Medioec <2103020@sit.singaporetech.edu.sg>
2021-11-11 11:22:58 +0800

    basic navbar, index.php, css

---
+++ navbar_inc.php
@@ -0,0 +1,46 @@
+<div class="nav-wrapper">
+    <nav class="navbar navbar-expand-xl navbar-dark bg-dark">
+        <a class="navbar-brand" href="#">
+            <img src="" alt="SITE LOGO"
+                 title="Home" height="30" width="30" />
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+            <ul class="navbar-nav mt-lg-0 navbar-col-1">
+                <li class="nav-item">
+                    <a class="nav-link" href="#dogs">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#cats">About</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                      Personal Banking
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                      <a class="dropdown-item" href="">Accounts</a>
+                      <a class="dropdown-item" href="">Loans</a>
+                      <a class="dropdown-item" href="">Cards</a>
+                    </div>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                      Business Banking
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                      <a class="dropdown-item" href="">Products</a>
+                      <a class="dropdown-item" href="">Financing</a>
+                      <a class="dropdown-item" href="">Corporate</a>
+                    </div>
+                </li>
+            </ul>
+            <ul class="navbar-nav mt-lg-0 navbar-col-2">
+                <li class="nav-item">
+                    <a class="nav-link" href="">Sign-in</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</div>
