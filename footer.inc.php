9d04065694a3e84604dd04e1a6a294ecdfa3b1e4
TheRealDexipher <76478315+TheRealDexipher@users.noreply.github.com>
2021-11-11 20:07:32 +0800

    footer.inc.php

---
+++ footer.inc.php
@@ -0,0 +1,46 @@
+<footer class="container">
+    <div class="footer">
+        <ul class='columns'>
+            <li>
+                <h4>Useful Links</h4>
+                <ul class='footer-list'>
+                    <li>
+                        <a href="" title="View Rates">View Rates</a>
+                    </li>
+                    <li>
+                        <a href="" title="Deposit Insurance Scheme">Deposit Insurance Scheme</a>
+                    </li>
+                    <li>
+                        <a href="" title="Terms & Condition">Terms and Conditions</a>
+                    </li>
+                </ul>
+            </li>
+            <li>
+                <h4>Getting Started</h4>
+                <ul class="footer-list">
+                    <li>
+                        <a href="" title="Introduction">Introduction</a>
+                    </li>
+                    <li>
+                        <a href="" title="Frequently Asked Questions">Frequently Asked Questions</a>
+                    </li>
+                </ul>
+            </li>
+            <li>
+                <h4>Contact Us</h4>
+                <ul class="footer-list">
+                    <li>
+                        <a href="" title="Email Us">Email Us</a>
+                    </li>
+                    <li>
+                        <a href="" title="Our Branches and ATMs">Our Branches & ATMs</a>
+                    </li>
+                    <li>
+                        <span>Singapore +65 1234 5678</span>
+                    </li>
+                </ul>
+            </li>
+        </ul>
+        <p class="copyright">Copyright &copy; 2020 Double'O4 Bank</p>
+    </div>
+</footer>
