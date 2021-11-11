6c97e9074ae9097697cf96896b88eacef261606f
grenishtri <93151690+grenishtri@users.noreply.github.com>
2021-11-11 11:48:54 +0800

    Add files via upload

---
+++ make_transaction.php
@@ -0,0 +1,11 @@
+<html>
+    <?php
+        include "head.inc.php";
+    ?>
+    <body>
+        <?php
+            include "nav.inc.php";
+        ?>
+        <p>Test string<p>
+    </body>
+</html>
