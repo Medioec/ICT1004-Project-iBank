b2222c1efa292943d960c616924c6d9b65470f73
S0meDev99 <90775954+S0meDev99@users.noreply.github.com>
2021-11-11 11:45:48 +0800

    PHP files for SQL connection and PHP Sessions
    -Using PDO instead of mysqli
    -Sessions file to automatically log out anyone without proper authentication.

---
+++ connect.php
@@ -0,0 +1,21 @@
+<?php
+	// Database connection variables and credentials
+        
+	$config = parse_ini_file('../../private/db-config.ini');
+	$servername = $config['servername'];
+	$username = $config['username'];
+	$password = $config['password'];
+	$dbname = $config['dbname'];
+        
+	try {
+		$connect = new PDO( "mysql:host=$servername;dbname=$dbname", $username, $password);
+		
+		$connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		$connect->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+		// echo "Connected successfully"; 
+	}
+	
+	catch(PDOException $e) {
+		echo "Connection failed: " . $e->getMessage();
+	}	
+?>
