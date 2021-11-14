b2535a24921246abf61e4dd0dd4fa3f035ea0ac4
siaoginna <90738474+mingwei1744@users.noreply.github.com>
2021-11-15 00:01:34 +0800

    Add files via upload
    - view_profile.php (READ and display user personal details)
    - allows user to update_profile.php with the option of changing password (UPDATE user details)
    - process_update_profile.php (UPDATING into DB and display successful message)

---
+++ process_update_profile.php
@@ -0,0 +1,21 @@
+<html>
+    <head>
+        <?php
+        include "head.inc.php";
+        ?>
+    </head>
+    <body>
+        <?php
+        include "nav.inc.php";
+        ?>  
+        <div class="page-bg"></div>
+        <div class="page-content">
+            <div class="main-content">
+
+                <h1>UNDER IMPLEMENTATION</h1>
+
+            </div>
+        </div>
+    </div>
+</body>
+</html>
