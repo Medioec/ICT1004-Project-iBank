2601f8d6d0dbb0ff747e9aa5f9a66135b7572f24
TheRealDexipher <76478315+TheRealDexipher@users.noreply.github.com>
2021-11-18 10:29:57 +0800

    Minor HTML Edits
    Added the general look of the about us page

--- about.php
+++ about.php
@@ -6,6 +6,37 @@
         <?php
             include "nav.inc.php";
         ?>
-        <p>Test string<p>
+        <div class='page-bg'></div>
+            <div class="page-body minw-500">
+                <div class="page-content minw-500">
+                    <header class="">
+                        <h1 class="">About Double'O4 Banking</h1>
+                        <h5>Always Listening, Always Understanding</h5>
+                    </header>                  
+                </div>
+                <div class="">    
+                    <main class="page-content minw-500">
+                        <section class="">
+                            <h2>History</h2>
+                            <p>Lorem ipsum dolor sit amet, consectetur 
+                                adipiscing elit. Cras quis nisl varius, posuere 
+                                erat sed, consectetur arcu. Suspendisse viverra 
+                                odio tincidunt felis rutrum, non commodo lorem 
+                                porttitor.</p>
+                        </section>
+                        <section class="">
+                            <h2>Milestoness</h2>
+                            <p>Lorem ipsum dolor sit amet, consectetur 
+                                adipiscing elit. Cras quis nisl varius, posuere 
+                                erat sed, consectetur arcu. Suspendisse viverra 
+                                odio tincidunt felis rutrum, non commodo lorem 
+                                porttitor.</p>
+                        </section>
+                    </main>
+                </div>
+            <?php
+                include "footer.inc.php";
+            ?>
+            </div>
     </body>
 </html>
