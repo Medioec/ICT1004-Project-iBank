a3520687d6d33ee1580e78bdf8b78b496608ad8b
TheRealDexipher <76478315+TheRealDexipher@users.noreply.github.com>
2021-11-18 13:30:45 +0800

    Added Carousel

--- index.php
+++ index.php
@@ -19,11 +19,34 @@ and open the template in the editor.
                         <h1 class="">Welcome to Double'O4 Banking</h1>
                     </header>                  
                 </div>
-                <div class="">    
+                <div class="page_carousel">  
                     <main class="page-content minw-500">
-                        <section class="">
-                            <h2>can add like a slideshow panel here</h2>
-                        </section>
+                        <div id="carousel" class="carousel slide" data-ride="carousel">
+                            <ol class="carousel-indicators">
+                                <li data-target="#carousel" data-slide-to="0" class="active"></li>
+                                <li data-target="#carousel" data-slide-to="1"></li>
+                                <li data-target="#carousel" data-slide-to="2"></li>
+                            </ol>
+                            <div class="carousel-inner">
+                                <div class="carousel-item active">
+                                    <img class="d-block w-100" src="images/abstract.jpg" alt="Abstract slide">
+                                </div>
+                                <div class="carousel-item">
+                                    <img class="d-block w-100" src="images/building.jpg" alt="Building slide">
+                                </div>
+                                <div class="carousel-item">
+                                    <img class="d-block w-100" src="images/clients.jpg" alt="client slide">
+                                </div>
+                            </div>
+                            <a class="carousel-control-prev" href="#carousel" role="button" data-slide="prev">
+                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                <span class="sr-only">Previous</span>
+                            </a>
+                            <a class="carousel-control-next" href="#carousel" role="button" data-slide="next">
+                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                <span class="sr-only">Next</span>
+                            </a>
+                        </div>
                     </main>
                 </div>
             <?php
