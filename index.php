53e3d87ae6cefb7ccd3abde63d5deb4333a59043
TheRealDexipher <76478315+TheRealDexipher@users.noreply.github.com>
2021-11-18 10:17:28 +0800

    Update index.php
    minor edits to the general look of the index page

--- index.php
+++ index.php
@@ -12,9 +12,23 @@ and open the template in the editor.
         <?php
             include "nav.inc.php";
         ?>
-        <p>Test string<p>
+        <div class='page-bg'></div>
+            <div class="page-body minw-500">
+                <div class="page-content minw-500">
+                    <header class="">
+                        <h1 class="">Welcome to Double'O4 Banking</h1>
+                    </header>                  
+                </div>
+                <div class="">    
+                    <main class="page-content minw-500">
+                        <section class="">
+                            <h2>can add like a slideshow panel here</h2>
+                        </section>
+                    </main>
+                </div>
+            <?php
+                include "footer.inc.php";
+            ?>
+            </div>
     </body>
-    <?php
-        include "footer.inc.php";
-    ?>
 </html>
