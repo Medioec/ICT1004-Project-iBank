a81dc1d33a1400441cd13018974dd610cd0d4473
S0meDev99 <90775954+S0meDev99@users.noreply.github.com>
2021-11-25 02:06:41 +0800

    Worked on User homepage
    -Implemented Chart.js to show Account Balances and User Transactions

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-<?php session_start();?>
 <!DOCTYPE html>
 <!--
 To change this license header, choose License Headers in Project Properties.
@@ -6,49 +5,29 @@ To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
 <html>
+    <script src="js/accountchart.js"></script>
+    
     <?php
+        session_start();    
         include "head.inc.php";
     ?>
+    
     <body>
         <?php
             include "nav.inc.php";
         ?>
         <div class='page-bg'></div>
             <div class="page-body minw-500">
-                <div class="header_content">
-                    <header class="">
-                        <h1 class="">Welcome to Double'O4 Banking</h1>
-                    </header>                  
-                </div>
-                <div class="page_carousel">  
-                    <main class="content">
-                        <div id="carousel" class="carousel slide" data-ride="carousel">
-                            <ol class="carousel-indicators">
-                                <li data-target="#carousel" data-slide-to="0" class="active"></li>
-                                <li data-target="#carousel" data-slide-to="1"></li>
-                                <li data-target="#carousel" data-slide-to="2"></li>
-                            </ol>
-                            <div class="carousel-inner">
-                                <div class="carousel-item active">
-                                    <img class="d-block w-100" src="images/abstract.jpg" alt="Abstract slide">
-                                </div>
-                                <div class="carousel-item">
-                                    <img class="d-block w-100" src="images/building.jpg" alt="Building slide">
-                                </div>
-                                <div class="carousel-item">
-                                    <img class="d-block w-100" src="images/clients.jpg" alt="client slide">
-                                </div>
-                            </div>
-                            <a class="carousel-control-prev" href="#carousel" role="button" data-slide="prev">
-                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                                <span class="sr-only">Previous</span>
-                            </a>
-                            <a class="carousel-control-next" href="#carousel" role="button" data-slide="next">
-                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                                <span class="sr-only">Next</span>
-                            </a>
-                        </div>
-                    </main>
+                <div class="page-content minw-500">
+                    <div>
+                        <?php
+                            if (isset($_SESSION["customerId"])) {
+                                include 'php/homepage.php';
+                            } else {
+                                include 'php/indexCarousel.php';
+                            }
+                        ?>
+                    </div>
                 </div>
             <?php
                 include "footer.inc.php";
