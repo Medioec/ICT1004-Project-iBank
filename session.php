0447d41467d569e38a53adee41ec3e35e3d8b2e2
Medioec <2103020@sit.singaporetech.edu.sg>
2021-11-25 11:53:12 +0800

    re enable redirect

--- session.php
+++ session.php
@@ -26,7 +26,7 @@
 	
 	if(($_SESSION['loggedin']!="1") || ($isActive=="0") || (!isset($isActive)) || ($_SESSION['otp']!="1")){
 		$_SESSION['loggedin']="0";
-		//header("location: login.php");
+		header("location: login.php");
     }
 
 	echo "<html oncontextmenu=\"return false\">";
