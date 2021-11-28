9bc65561b7f0930b087f39d244ac27d9502a2339
TheRealDexipher <76478315+TheRealDexipher@users.noreply.github.com>
2021-11-28 13:56:30 +0800

    Update about.php

--- about.php
+++ about.php
@@ -10,44 +10,57 @@
         <div class='page-bg'></div>
             <div class="page-body minw-500">
                 <div class="header_content">
-                    <header class="">
-                        <h1 class="about-header">About Double'O4 Banking</h1>
+                    <header class="about_header">
+                        <h1>About Double'O4 Banking</h1>
                         <h5>Always Listening, Always Understanding</h5>
-                        
-                        <p><br>Double'O4 is committed to building lasting relationships 
-                        with our customers, through product and market expertise, 
-                        and our promise to always do what is right.</p>
-                        
-                        <p>With a well-established global presence today and 
-                        particularly in Asia, Double'O4 has understanding of Asian 
-                        markets, corporate culture and business mindsets, which 
-                        is matched by few. Our strong foothold in Singapore, 
-                        Malaysia, Indonesia, Thailand and China is well-placed 
-                        to create greater access and growth in this region, for 
-                        our customers.</p>
                     </header>
-                </div>
-                <div class="about-content">    
+                </div> 
+                <div class="about_content">
                     <main class="content">
-                        <section class="about-section">
-                            <img class="about-imgs" src="images/history.jpg" width = "330" height ="300" alt="History">
-                            <h2>History</h2>
-                            <p>It all began during the fall of 2021 where 5 guys, Eric, Alford, Wee Yee, Ming Wei and Prince came together one night 
-                            after a good ol' hearty meal decided to embark on a journey few men has ever sought to build a magical bank made with magical touches.
-                            These touches are straight from the heart of these brave young men.</p>
-                        </section>
-                        <section class="about-section">
-                            <img class="about-imgs" src="images/business.jpg" width = "330" height ="300" alt="Business">
-                            <h2>Our Business</h2>
-                            <p>Our business offers a wide range of variety from insurance to keeping your loved one's safe.</p>
-                        </section>
-                        <section class="about-section">
-                            <img class="about-imgs" src="images/community.jpg" width = "330" height ="300" alt="Community">
-                            <h2>Community Involvement</h2>
-                            <p>We have helped those in need and removes interests to those who are need of money and gives them the flexibility to pay back whenever 
-                            they can within a specific time frame in which they decide. The only interest free loan bank.</p>
-                        </section>
+                        <div class="general_containers">
+                            <p><br>Double'O4 is committed to building lasting relationships 
+                            with our customers, through product and market expertise, 
+                            and our promise to always do what is right.</p>
+
+                            <p>With a well-established global presence today and 
+                            particularly in Asia, Double'O4 has understanding of Asian 
+                            markets, corporate culture and business mindsets, which 
+                            is matched by few. Our strong foothold in Singapore, 
+                            Malaysia, Indonesia, Thailand and China is well-placed 
+                            to create greater access and growth in this region, for 
+                            our customers.</p>
+                        </div>
                     </main>
+                </div>
+                <div class="about_content">
+                    <div class="content">
+                        <div class="general_containers">
+                            <section class="about_section">
+                                <img class="about_imgs" src="images/history.jpg" alt="History">
+                                <h2>History</h2>
+                                <p>It all began during the fall of 2021 where 5 guys, 
+                                    Eric, Alford, Wee Yee, Ming Wei and Prince came together 
+                                    one night after a good ol' hearty meal decided to embark 
+                                    on a journey few men has ever sought to build a magical bank 
+                                    made with magical touches. These touches are straight from 
+                                    the heart of these brave young men.</p>
+                            </section>
+                            <section class="about_section">
+                                <img class="about_imgs" src="images/business.jpg" alt="Business">
+                                <h2>Our Business</h2>
+                                <p>Our business offers a wide range of variety from insurance to 
+                                    keeping your loved one's safe.</p>
+                            </section>
+                            <section class="about_section">
+                                <img class="about_imgs" src="images/community.jpg" alt="Community">
+                                <h2>Community Involvement</h2>
+                                <p>We have helped those in need and removes interests to those who 
+                                    are need of money and gives them the flexibility to pay back whenever 
+                                    they can within a specific time frame in which they decide. The only 
+                                    interest free loan bank.</p>
+                            </section>
+                        </div>
+                    </div>
                 </div>
             <?php
                 include "footer.inc.php";
