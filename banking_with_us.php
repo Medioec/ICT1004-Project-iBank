7c865557aad0da89cb0fad2731c64360d700309e
TheRealDexipher <76478315+TheRealDexipher@users.noreply.github.com>
2021-11-28 13:59:14 +0800

    Update banking_with_us.php

--- banking_with_us.php
+++ banking_with_us.php
@@ -11,7 +11,7 @@
             <div class="page-body minw-500">
                 <div class="general_header">
                     <header>
-                        <h1>Bank with Double'O4</h1>
+                        <h1 class='about_header'>Bank with Double'O4</h1>
                         <p>Run your business efficiently with our 
                             24/7 digital banking, islandwide branches, and dedicated SME resources.</p>
                     </header>
@@ -19,7 +19,7 @@
                 <div class="general_content">
                     <h3 class="about_header">Ways to Bank</h3>
                     <main class="content">
-                        <section class="about_section">
+                        <section class="bwu_section">
                             <div class="card" style="width: 20rem;">
                                 <img class="card-img-top" src="images/onlinebanking.jpg" alt="Internet Banking">
                                 <div class="card-body">
@@ -29,7 +29,7 @@
                                 </div>
                             </div>
                         </section>
-                        <section class="about_section">
+                        <section class="bwu_section">
                             <div class="card" style="width: 20rem;">
                                 <img class="card-img-top" src="images/tradecounters.jpg" alt="Trade Counters">
                                 <div class="card-body">
@@ -39,7 +39,7 @@
                                 </div>
                             </div>
                         </section>
-                        <section class="about_section">
+                        <section class="bwu_section">
                             <div class="card" style="width: 20rem;">
                                 <img class="card-img-top" src="images/atm.jpg" alt="ATMs & Branches">
                                 <div class="card-body">
