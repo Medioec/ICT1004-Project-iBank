3d0997fbec487b73b56f30acda18d9ba50b0685a
TheRealDexipher <76478315+TheRealDexipher@users.noreply.github.com>
2021-11-28 14:01:21 +0800

    Update nav.inc.php

--- nav.inc.php
+++ nav.inc.php
@@ -33,8 +33,8 @@
                         </a>
                         <div class="dropdown-menu" aria-labelledby="businessBankingList">
                         <a class="dropdown-item" href="banking_with_us.php">Banking with us</a>
-                        <a class="dropdown-item" href="">Financing</a>
-                        <a class="dropdown-item" href="">Corporate</a>
+                        <a class="dropdown-item" href="finance.php">Financing</a>
+                        <a class="dropdown-item" href="trade_service.php">Trade Services</a>
                         </div>
                     </li>
                     ';
