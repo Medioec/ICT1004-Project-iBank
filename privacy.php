98399d0adb0e8c3be5ba9baf9b58f08bd28923f6
TheRealDexipher <76478315+TheRealDexipher@users.noreply.github.com>
2021-12-01 16:33:41 +0800

    Added Privacy Policy Page

---
+++ privacy.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html lang="en">
+    <?php include "head.inc.php";?>
+    <body>
+        <?php include "nav.inc.php";?>
+        <div class='page-bg'></div>
+        <div class="carousel-container">
+            <div class="page_carousel">
+                <div id="carousel" class="carousel slide" data-ride="carousel">
+                    <ol class="carousel-indicators">
+                        <li data-target="#carousel" data-slide-to="0" class="active"></li>
+                        <li data-target="#carousel" data-slide-to="1"></li>
+                        <li data-target="#carousel" data-slide-to="2"></li>
+                    </ol>
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img class="d-block w-100" src="images/abstract.jpg" alt="Abstract slide">
+                        </div>
+                        <div class="carousel-item">
+                            <img class="d-block w-100" src="images/building.jpg" alt="Building slide">
+                        </div>
+                        <div class="carousel-item">
+                            <img class="d-block w-100" src="images/clients.jpg" alt="client slide">
+                        </div>
+                    </div>
+                    <a class="carousel-control-prev" href="#carousel" role="button" data-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="carousel-control-next" href="#carousel" role="button" data-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Next</span>
+                    </a>
+                </div>
+            </div>
+        </div>
+        <main class="index-page-body minw-500">
+            <div class="index-page-content minw-500">
+                <div class="container-fluid info-main-content">
+                    <div class="jumbotron jumbotitle jumbotron-fluid mt-5">
+                        <div class="container">
+                            <h1 class="display-4">Privacy Policy</h1>
+                            <p class="lead">Double'O4 ensures the privacy of our clients are not leaked in any manner.</p>
+                        </div>
+                    </div>
+                    
+                    <div class="jumbotron jumbotron-fluid mt-5">
+                        <div class="container">
+                            <div class="row">
+                            <h1 class="display-4 col-md-5">Trust in Us</h1>
+                            <p class="lead col">We will uphold your trust in us by protecting and using your personal data in a responsible manner.</p>
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <div class="jumbotron jumbotron-fluid mt-5">
+                        <div class="container">
+                            <div class="row">
+                                <ul class="lead col">
+                                    <li>Personal Particulars (e.g. name, address, DOB)</li>
+                                    <li>Tax, Insurance & Employment Details</li>
+                                    <li>Your Banking Information (Images, Biometrics, Voice Recordings)</li>
+                                    <li>Information From Mobile Device with your Consent</li>
+                                </ul>
+                                <h1 class="display-4 col-md-5">Types of Data</h1>
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <div class="jumbotron jumbotron-fluid mt-5">
+                        <div class="container">
+                            <div class="row">
+                                <h1 class="display-4 col-md-5">Collection of Data</h1>
+                                <ul class="lead col">
+                                    <li>Online & Digital</li>
+                                    <li>Cashless Transactions</li>
+                                    <li>Offline Interactions</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <div class="jumbotron jumbotron-fluid mt-5">
+                        <div class="container">
+                            <div class="row">
+                                <ul class="lead col">
+                                    <li>For General Support</li>
+                                    <li>For Our Internal Operations</li>
+                                    <li>For Marketing Purposes</li>
+                                </ul>
+                                <h1 class="display-4 col-md-5">Data Usage</h1>
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <div class="jumbotron jumbotron-fluid mt-5">
+                        <div class="container">
+                            <div class="row">
+                                <h1 class="display-4 col-md-5">Data Sharing</h1>
+                                <ul class="lead col">
+                                    <h4><b>Who:</b></h4>
+                                    <li>Third Parties like Campaign Partners</li>
+                                    <li>Carefully Selected Partners</li>
+                                    <li>Relevant Authorities like the Government</li>
+                                </ul>
+                                <ul class="lead col">
+                                    <h4><b>How:</b></h4>
+                                    <li>Requiring all personnel and third parties to ensure that any of the data disclosed
+                                    to them is kept confidential and secure.</li>
+                                    <li>We do not sell your Personal Data to any third party and we shall comply fully with 
+                                        any duty or obligation of confidentiality that governs our relationship with you.</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </main>
+        <?php include "footer.inc.php";?>
+    </body>
+</html>
