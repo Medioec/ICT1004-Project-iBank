c0868d83a5e3e0ee55f7db6e6863c1708ea7064a
Medioec <2103020@sit.singaporetech.edu.sg>
2021-12-03 19:09:42 +0800

    fix infinite scrolling bug, add logging

--- js/main.js
+++ js/main.js
@@ -1,18 +1,24 @@
 $(document).ready(function() {
-    $('#example').DataTable( {
-        "order": [[ 0, "desc" ]]
+    $('#example').DataTable({
+        "order": [
+            [0, "desc"]
+        ]
     });
-    $(".form-validate").on("click", ".submit-button", function (e) 
-        {
+    $(".form-validate").on("click", ".submit-button", function(e) {
             validateForm(e);
-        }
-    )
-    //side menu scrollposition code
-    $(window).scroll(function() 
-    {
+        })
+        //side menu scrollposition code
+    $(window).scroll(function() {
+        var content = document.getElementsByClassName("info-main-content")[0];
+        if (!content) content = document.getElementsByClassName("main-content")[0];
+        if (!content) return;
         var menu = document.getElementsByClassName("front-side-menu")[0];
-        if (menu){
-            if (window.scrollY > 349) {
+
+        var domRect = content.getBoundingClientRect();
+
+        if (menu) {
+            if (domRect.bottom - window.innerHeight < 0);
+            else if (window.scrollY > 349) {
                 var position = window.scrollY - 350 + 100;
                 menu.style.marginTop = position.toString() + "px";
             } else if (window.scrollY < 351) {
@@ -20,21 +26,17 @@ $(document).ready(function() {
                 menu.style.marginTop = position.toString() + "px";
             }
         }
-    }
-    );
-    $(window).scroll(function() 
-    {
         var menu = document.getElementsByClassName("user-side-menu")[0];
-        if (menu){
-            if (window.scrollY != 0) {
+        if (menu) {
+            if (domRect.bottom - window.innerHeight < 0);
+            else if (window.scrollY != 0) {
                 var position = window.scrollY + 100;
                 menu.style.marginTop = position.toString() + "px";
             }
         }
-    }
-    );
+    });
     activateMenu();
-} );
+});
 
 //Validates forms using bootstrap styling
 function validateForm(e) {
@@ -75,33 +77,27 @@ function goHome() {
 
 // Grid Function
 function openTab(tabName) {
-  var i, x;
-  x = document.getElementsByClassName("containerTab");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
-  }
-  document.getElementById(tabName).style.display = "block";
+    var i, x;
+    x = document.getElementsByClassName("containerTab");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    document.getElementById(tabName).style.display = "block";
 }
 
-function activateMenu()
-{
+function activateMenu() {
     setTimeout
-    (() => 
-        {
+        (() => {
             var current_page_URL = location.href;
-            $("a.nav-link").each
-            (
-                function ()
-                {
+            $("a.nav-link").each(
+                function() {
                     var target_URL = $(this).prop("href");
-                    if (target_URL === current_page_URL)
-                    {
+                    if (target_URL === current_page_URL) {
                         $('nav a').parents('li, ul').removeClass('active');
                         $(this).addClass('active');
                         return false;
                     }
                 }
             );
-        }, 1
-    );
+        }, 1);
 }
--- php/accountCreationHandler.php
+++ php/accountCreationHandler.php
@@ -1,5 +1,15 @@
 <?php
 function createAccount($connect) {
+
+    $logSql = "INSERT INTO `log`(`type`,`category`, `description`, `user_performed`, `timestamp`) VALUES (?,?,?,?,CURRENT_TIMESTAMP)";
+    $logType0 = "BANKING";
+    $logType1 = "BANKING FAIL";
+    $logCategory0 = "INFO";
+    $logCategory1 = "WARNING";
+    $description = "";
+    $table1InsertFail = 0;
+    $table2InsertFail = 0;
+
     $accountType = sanitize_input($_POST["createType"]);
     $valid = $accountType == "Savings"||$accountType == "Checking";
     $success = 1;
@@ -8,14 +18,19 @@ function createAccount($connect) {
         $stmt = $connect->prepare($action);
         $stmt->bindParam(1, $accountType, PDO::PARAM_STR);
         try {
-            $stmt->execute();
+            $success = $stmt->execute();
         }
         catch(PDOException $e) {
             //echo "Retrieve failed: " . $e->getMessage();
             $success = 0;
+        }
+
+        if (!$success) {
+            $table1InsertFail = 1;
             $_SESSION["sqlFailed"] = 1;
             header("Location: request_error.php");
         }
+
         if ($success) {
             $newAccountId = $connect->lastInsertId();
             $action = "INSERT INTO `bank_accounts_ref`(`customer_id`,`account_id`) VALUES (?,?);";
@@ -24,17 +39,53 @@ function createAccount($connect) {
             $stmt->bindParam(2, $newAccountId, PDO::PARAM_STR);
 
             try {
-                $stmt->execute();
+                $success = $stmt->execute();
             }
             catch(PDOException $e) {
                 //echo "Retrieve failed: " . $e->getMessage();
                 $success = 0;
+            }
+
+            if (!$success) {
+                $table2InsertFail = 1;
                 $_SESSION["sqlFailed"] = 1;
                 header("Location: request_error.php");
             }
 
+            if ($table2InsertFail && !$table1InsertFail) {
+                //Create fail log
+                $action = $logSql;
+                $description = "SQL FAILURE (Please escalate to db admin)";
+                $stmt = $connect->prepare($action);
+                $stmt->bindParam(1, $logType1, PDO::PARAM_STR);
+                $stmt->bindParam(2, $logCategory1, PDO::PARAM_STR);
+                $stmt->bindParam(3, $description, PDO::PARAM_STR);
+                $stmt->bindParam(4, $_SESSION["username"], PDO::PARAM_STR);
+                try {
+                    $stmt->execute();
+                }
+                catch(PDOException $e) {
+                    //echo "Retrieve failed: " . $e->getMessage();
+                }
+            }
+
         }
         if ($success) {
+            //Create success log
+            $action = $logSql;
+            $description = "BANK ACCOUNT ".$newAccountId." CREATED";
+            $stmt = $connect->prepare($action);
+            $stmt->bindParam(1, $logType0, PDO::PARAM_STR);
+            $stmt->bindParam(2, $logCategory0, PDO::PARAM_STR);
+            $stmt->bindParam(3, $description, PDO::PARAM_STR);
+            $stmt->bindParam(4, $_SESSION["username"], PDO::PARAM_STR);
+            try {
+                $stmt->execute();
+            }
+            catch(PDOException $e) {
+                //echo "Retrieve failed: " . $e->getMessage();
+            }
+
             $_SESSION["sqlSuccess"] = 1;
             $_SESSION["newAccountType"] = $accountType;
             $_SESSION["newAccountId"] = $newAccountId;
